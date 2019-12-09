eeaf77ca90f774ff01678802cb3fa803c668e467
Anand Graves <anandgraves@users.noreply.github.com>
2019-12-09 21:47:27 +0100

    refactor(products): route for productId

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,17 +2,31 @@ import { Router } from 'express'
 import db from '../../db/index'
 const router = Router()
 
-router.get('/products', async function(req, res, next) {
-  const q = {
-    // give the query a unique name
+router.get('/products', async function(req, res) {
+  const query = {
     name: 'fetch-products',
-    text: 'SELECT * FROM products',
+    text: 'SELECT * FROM products ORDER BY id LIMIT 20'
   }
+
   try {
-    const dbResponse = await db.query(q)
-  res.json(dbResponse.rows)
+    const dbResponse = await db.query(query)
+    res.json(dbResponse.rows)
+  } catch (error) {
+    console.error('oops, db foutje', error)
   }
-  catch(error) {
+})
+
+router.get('/products/:productId', async function(req, res) {
+  const productId = req.params.productId
+  const query = {
+    name: 'fetch-product',
+    text: `SELECT * FROM products WHERE id=${productId}`
+  }
+
+  try {
+    const dbResponse = await db.query(query)
+    res.json(dbResponse.rows[0])
+  } catch (error) {
     console.error('oops, db foutje', error)
   }
 })
